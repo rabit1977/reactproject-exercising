fcee90509f830aeee83526fd5aca5b78844195b7
Rabit <rabit2245@gmail.com>
2022-01-29 00:19:23 +0100

    added h1 title and two paragraphs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className='App'>
-      <h1>The React Exercising Project</h1>
+    <div className='bg-red-300 h-screen'>
+      <div>
+        <h1 className='text-5xl flex justify-center py-8'>
+          First Website from Me
+        </h1>
+        <div className='bg-blue-300 flex space-x-4 h-auto p-4'>
+          <div>
+            <h1 className='text-3xl mb-2 font-bold underline'>Hello world!</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
+              accusamus rem suscipit laborum fugit. Cumque, voluptas? Suscipit
+              neque blanditiis doloribus rerum accusantium facilis placeat porro
+              soluta. Maiores quas illo nulla distinctio! Similique quibusdam
+              sint sed fuga asperiores harum incidunt, provident corporis minus,
+              libero tempora quam dignissimos ratione magni? Sunt, quam.
+            </p>
+          </div>
+          <div>
+            <h1 className='text-3xl mb-2 font-bold underline'>Hello world!</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
+              accusamus rem suscipit laborum fugit. Cumque, voluptas? Suscipit
+              neque blanditiis doloribus rerum accusantium facilis placeat porro
+              soluta. Maiores quas illo nulla distinctio! Similique quibusdam
+              sint sed fuga asperiores harum incidunt, provident corporis minus,
+              libero tempora quam dignissimos ratione magni? Sunt, quam.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
